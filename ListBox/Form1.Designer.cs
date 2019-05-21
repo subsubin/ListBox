c7587f1d2bbbd9612a420760827e9ed01af618bd
Subin Kim <s2017s24@e-mirim.hs.kr>
2019-05-21 11:50:24 +0900

    #1 화면 디자인 구성(1)

--- ListBox/Form1.Designer.cs
+++ ListBox/Form1.Designer.cs
@@ -28,13 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtList = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.lbView = new System.Windows.Forms.ListBox();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtList
+            // 
+            this.txtList.Location = new System.Drawing.Point(12, 252);
+            this.txtList.Name = "txtList";
+            this.txtList.Size = new System.Drawing.Size(370, 21);
+            this.txtList.TabIndex = 0;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(12, 289);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(370, 45);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "추가";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // lbView
+            // 
+            this.lbView.FormattingEnabled = true;
+            this.lbView.ItemHeight = 12;
+            this.lbView.Location = new System.Drawing.Point(12, 12);
+            this.lbView.Name = "lbView";
+            this.lbView.Size = new System.Drawing.Size(370, 196);
+            this.lbView.TabIndex = 2;
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(12, 228);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(41, 12);
+            this.lblResult.TabIndex = 3;
+            this.lblResult.Text = "결과 : ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(394, 346);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.lbView);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtList);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "리스트 추가";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtList;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.ListBox lbView;
+        private System.Windows.Forms.Label lblResult;
     }
 }
 
